fa81385f061160391e41334eb241bebb96e03305
Kyle <kylesk22@gmail.com>
2024-04-05 21:49:23 -0400

    test

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -3,6 +3,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 //include your index.scss file into the bundle
+import "../styles/jquery"
+import "../styles/jquery.fancybox"
+import "../styles/jquery-ui"
+import "../styles/jquery.form.min"
+import "../styles/jquery.validate.min"
+import "../styles/script"
+import "../styles/fontawesome.css"
+import "../styles/progress-bar"
+import "../styles/popper.min"
+import "../styles/mixitup"
+import "../styles/wow"
+import "../styles/slick.min"
+import "../styles/slick-animation.min"
+import "../styles/appear"
+
+
+
+
 import "../styles/index.css";
 import "../../../bootstrap.min.css"
 import "../styles/slick-theme.css"
@@ -70,20 +88,20 @@ import "../fonts/linearicons-free.eot"
 import "../fonts/linearicons-free.ttf"
 import "../fonts/linearicons-free.woff"
 
-import "../styles/jquery"
-import "../styles/jquery.fancybox"
-import "../styles/jquery-ui"
-import "../styles/jquery.form.min"
-import "../styles/jquery.validate.min"
-import "../styles/script"
-import "../styles/fontawesome.css"
-import "../styles/progress-bar"
-import "../styles/popper.min"
-import "../styles/mixitup"
-import "../styles/wow"
-import "../styles/slick.min"
-import "../styles/slick-animation.min"
-import "../styles/appear"
+// import "../styles/jquery"
+// import "../styles/jquery.fancybox"
+// import "../styles/jquery-ui"
+// import "../styles/jquery.form.min"
+// import "../styles/jquery.validate.min"
+// import "../styles/script"
+// import "../styles/fontawesome.css"
+// import "../styles/progress-bar"
+// import "../styles/popper.min"
+// import "../styles/mixitup"
+// import "../styles/wow"
+// import "../styles/slick.min"
+// import "../styles/slick-animation.min"
+// import "../styles/appear"
 
 import DomineBold from "../fonts/Domine Bold.ttf"
 

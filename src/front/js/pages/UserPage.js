d1ceb73ac057d5bbf5ef1d4809bb547f1816c79d
Kyle <kylesk22@gmail.com>
2024-04-16 16:05:12 -0400

    test

--- src/front/js/pages/UserPage.js
+++ src/front/js/pages/UserPage.js
@@ -165,12 +165,12 @@ export const UserPage = props => {
         <div >
             {(sessionStorage.getItem("id"))?
             <div>
-            <div className="row">
+            <div className="row" style={{position: "relative", marginTop:"150px"}}>
                 <div className="col-12 text-center">
                     <h1>Welcome Dr. {firstName}</h1>
                 </div>
             </div>
-            <div>
+            <div style={{marginBottom: "500px"}}>
                 <SideBar page={page} handleGetPage={getPage} getAllCases={getCaseInfo}/>
 
                 {(page === "home")?

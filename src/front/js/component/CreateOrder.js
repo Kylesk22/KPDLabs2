d2bdb2517572fc4cfdb5eedb5109b1e9af7ccf29
Kyle <kylesk22@gmail.com>
2024-04-18 15:52:02 -0400

    test

--- src/front/js/component/CreateOrder.js
+++ src/front/js/component/CreateOrder.js
@@ -13,7 +13,7 @@ export const CreateOrder = props => {
 const s3Client = new S3Client({
     endpoint: "https://case-scans.nyc3.cdn.digitaloceanspaces.com", // Find your endpoint in the control panel, under Settings. Prepend "https://".
     forcePathStyle: false, // Configures to use subdomain/virtual calling format.
-    region: "us-east-1", // Must be "us-east-1" when creating new Spaces. Otherwise, use the region in your endpoint (for example, nyc3).
+    region: "nyc3", // Must be "us-east-1" when creating new Spaces. Otherwise, use the region in your endpoint (for example, nyc3).
     credentials: {
         accessKeyId: process.env.SPACES_KEY, // Access key pair. You can create access key pairs using the control panel or API.
         secretAccessKey: process.env.SPACES_SECRET_KEY // Secret access key defined through an environment variable.
@@ -23,7 +23,7 @@ const s3Client = new S3Client({
 
 const params = {
     Bucket: "case-scans", // The path to the directory you want to upload the object to, starting with your Space name.
-    Key: `${caseNum}/${stlFile.name}`, // Object key, referenced whenever you want to access this file later.
+    Key: `${caseNum}/${stlFile}`, // Object key, referenced whenever you want to access this file later.
     Body: "Hello, World!", // The object's contents. This variable is an object, not a string.
     ACL: "private", // Defines ACL permissions, such as private or public.
     Metadata: { // Defines metadata tags.

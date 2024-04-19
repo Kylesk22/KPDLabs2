b92c453a66107766fc337544ccaa6a13c6b64786
Kyle <kylesk22@gmail.com>
2024-04-19 09:53:44 -0400

    test

--- src/front/js/component/CreateOrder.js
+++ src/front/js/component/CreateOrder.js
@@ -49,12 +49,13 @@ const s3Client = new S3Client({
 
   const uploadObject = async () => {
     try {
+        for (let i = 0; i < stlFile.length; i++) {
         const params = {
     
 
             Bucket: "case-scans", // The path to the directory you want to upload the object to, starting with your Space name.
-            Key: `${caseNum}/${stlFile[0].name}`, // Object key, referenced whenever you want to access this file later.
-            Body: stlFile[0], // The object's contents. This variable is an object, not a string.
+            Key: `${caseNum}/${stlFile[i].name}`, // Object key, referenced whenever you want to access this file later.
+            Body: stlFile[i], // The object's contents. This variable is an object, not a string.
             ACL: "private", // Defines ACL permissions, such as private or public.
             Metadata: { // Defines metadata tags.
               "x-amz-meta-my-key": `${caseNum}`
@@ -74,7 +75,7 @@ const s3Client = new S3Client({
           params.Key
       );
       return data;
-    } catch (err) {
+    }} catch (err) {
       console.log("Error", err);
     }
   };

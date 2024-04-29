48c2e19ed771cb47656592573eac40c1ffabf419
Kyle <kylesk22@gmail.com>
2024-04-29 12:14:13 -0400

    test

--- src/front/js/component/UserCases.js
+++ src/front/js/component/UserCases.js
@@ -74,9 +74,9 @@ export const UserCases = props => {
                             <div key={index} className="row" onClick={()=>{setSinglePage("singleCase"), props.setSingleCaseID(item["id"])}}>
                                 {(index <= pageMax && index >= pageMin)?
                                 <>
-                                <div className = "col-2 text-center" style={{border: "solid rgba(0, 0, 0, .125) 1px", color:"black", backgroundColor:"white"}} >{item["id"]}</div>
-                                <div className = "col-5 text-center" style={{border: "solid rgba(0, 0, 0, .125) 1px", color:"black", backgroundColor:"white"}}>{item["name"]}</div>
-                                <div className = "col-3 text-center" style={{border: "solid rgba(0, 0, 0, .125) 1px", color:"black", backgroundColor:"white"}}>{item["status"]}</div>
+                                <div className = "col-md-2 col-3  text-center" style={{border: "solid rgba(0, 0, 0, .125) 1px", color:"black", backgroundColor:"white"}} >{item["id"]}</div>
+                                <div className = "col-md-5 col-6 text-center" style={{border: "solid rgba(0, 0, 0, .125) 1px", color:"black", backgroundColor:"white"}}>{item["name"]}</div>
+                                <div className = "col-md-3 col-3 text-center" style={{border: "solid rgba(0, 0, 0, .125) 1px", color:"black", backgroundColor:"white"}}>{item["status"]}</div>
                                 
                                 </>
                                 

e3fc310f7303d8a35f22b402ba9ef00c37847658
Kyle <kylesk22@gmail.com>
2024-05-08 10:58:53 -0400

    test

--- src/front/js/pages/UserPage.js
+++ src/front/js/pages/UserPage.js
@@ -189,7 +189,7 @@ export const UserPage = props => {
             <div style={{backgroundImage: `url(${AboutBKG})`}}>
             <div className="row" style={{paddingTop: "150px"}}>
                 <div className="col-12 user-header" style={{minHeight: "157px"}}>
-                    <h3 style={{paddingTop: "50px"}}>Welcome Dr. {lastName}</h3>
+                    <h3 style={{paddingTop: "50px"}}>Welcome Dr. {firstNameLower} {lastName}</h3>
                 </div>
             </div>
             <div  style={{paddingBottom: "500px"}}>

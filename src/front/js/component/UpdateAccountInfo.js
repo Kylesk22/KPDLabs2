ba17eea91230ed0bb92b523a5ba9778c6be242ef
Kyle <kylesk22@gmail.com>
2024-05-10 13:49:43 -0400

    test

--- src/front/js/component/UpdateAccountInfo.js
+++ src/front/js/component/UpdateAccountInfo.js
@@ -85,7 +85,7 @@ export const UpdateAccountInfo = props => {
                 <div className="form-group mx-3">
                         <label htmlFor="streetAddress" className="form-label mt-4 " style={{textAlign: "center", color:"black"}} >Street Address</label>
                         <input className="form-control" id="streetAddress" readOnly type="text" style={{borderRadius: "1rem", minHeight:"40px"}}  value={streetAddress} ></input>
-                        <button onClick={()=>setEditStreet(true)}></button>
+                        <button className="btn btn-primary mb-4 mx-auto" onClick={()=>setEditStreet(true)}></button>
                         {(editStreet)?
                             <input style={{backgroundColor: "white", border: "black 1px solid"}} type="text" required className="form-control" id="streetAddress" placeholder="StreetAddress" value={streetAddress} onChange={(e)=>setStreetAddress(e.target.value)} />
                         :""}
@@ -93,7 +93,7 @@ export const UpdateAccountInfo = props => {
                     <div className="form-group mx-3">
                         <label htmlFor="city" className="form-label mt-4 " style={{textAlign: "center", color:"black"}}>City</label>
                         <input className="form-control" id="city" readOnly type="text" style={{borderRadius: "1rem", minHeight:"40px"}}  value={city} ></input>
-                        <button onClick={()=>setEditCity(true)}></button>
+                        <button className="btn btn-primary mb-4 mx-auto" onClick={()=>setEditCity(true)}></button>
                         {(editCity)?
                             <input type="text" style={{backgroundColor: "white", border: "black 1px solid"}} required className="form-control" id="city" placeholder="City" value={city} onChange={(e)=>setCity(e.target.value)}/>
                         :""}
@@ -103,7 +103,7 @@ export const UpdateAccountInfo = props => {
                         <label htmlFor="stateSelect" className="form-label mt-4" style={{color:"black"}}>State</label>
                         <br></br>
                         <input className="form-control" id="state" readOnly type="text" style={{borderRadius: "1rem", minHeight:"40px"}}  value={stateSelect} ></input>
-                        <button onClick={()=>setEditState(true)}></button>
+                        <button className="btn btn-primary mb-4 mx-auto" onClick={()=>setEditState(true)}></button>
                         {(editState)?
                             <SelectUSState id="stateSelect"  required className="form-control states" onChange={(e)=>{setStateSelect(e)}} value={stateSelect}>
                             </SelectUSState>
@@ -112,7 +112,7 @@ export const UpdateAccountInfo = props => {
                     <div className="form-group mx-3">
                         <label htmlFor="zip" className="form-label mt-4 " style={{textAlign: "center", color:"black"}}>Zip Code</label>
                         <input className="form-control" id="city" readOnly type="text" style={{borderRadius: "1rem", minHeight:"40px"}}  value={zip} ></input>
-                        <button onClick={()=>editZip(true)}></button>
+                        <button className="btn btn-primary mb-4 mx-auto" onClick={()=>editZip(true)}></button>
                         {(editZip)?
                             <input type="text" style={{backgroundColor: "white", border: "black 1px solid"}} required className="form-control" id="zip" placeholder="Zip Code" value={zip} onChange={(e)=>setZip(e.target.value)}/>
                         :""}

0d2daf02d8daec0cea66a1a97bcea1e8335af135
Kyle <kylesk22@gmail.com>
2024-05-14 15:59:48 -0400

    test

--- src/api/routes.py
+++ src/api/routes.py
@@ -59,22 +59,7 @@ def assign_access_refresh_tokens(email, url):
     set_refresh_cookies(resp, refresh_token)
     return resp
 
-def admin_required():
-    def wrapper(fn):
-        @wraps(fn)
-        def decorator(*args, **kwargs):
-            verify_jwt_in_request()
-            claims = get_jwt()
-            print(claims)
-            print(get_jwt_identity())
-            if claims["is_administrator"]:
-                return fn(*args, **kwargs)
-            else:
-                return jsonify(msg="Admins only!"), 403
 
-        return decorator
-
-    return wrapper
 
 
 
@@ -161,7 +146,7 @@ def admin_login():
         return jsonify({'message': 'Invalid username or password'}), 401
     
 @api.route('/admin/<int:id>', methods=['GET'])
-@admin_required()
+@jwt_required()
 def getAllInfo(id):
     current_user_email = get_jwt_identity()
     

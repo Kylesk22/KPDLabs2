e4b0f52874738716915ab1ba9ea5332d490c2d26
Kyle <kylesk22@gmail.com>
2024-05-17 11:41:25 -0400

    test

--- src/front/js/pages/AdminSingleCase.js
+++ src/front/js/pages/AdminSingleCase.js
@@ -517,7 +517,7 @@ export const AdminSingleCase = props => {
                             
                     )})}
                 </div> */}
-               
+               <button onClick={()=>console.log(rates)}>Test</button>
                 
                 
             

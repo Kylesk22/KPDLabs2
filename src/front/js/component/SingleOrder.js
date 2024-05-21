a3b14107a0f4614f435098cd1127ccdb2b669df6
Kyle <kylesk22@gmail.com>
2024-05-21 10:00:12 -0400

    test

--- src/front/js/component/SingleOrder.js
+++ src/front/js/component/SingleOrder.js
@@ -101,7 +101,9 @@ export const SingleOrder = props => {
                     console.log(data)
                     setPatientName(data.name);
                     setCaseNum(data.id);
-                    setCrownTooth(data.teeth);
+                    returnedTeeth = data.teeth
+                    const numberArray = JSON.parse("[" + returnedTeeth.replace(/"/g, "") + "]");
+                    setCrownTooth(numberArray);
                     for (let tooth in crownTooth){
                         const element = document.getElementById(tooth);
                         element.style.fill = "#137ea7"

16460cfcf1693d2bfd5e84b7fa96dfc57fba7252
Kyle <kylesk22@gmail.com>
2024-07-12 11:28:47 -0400

    test

--- src/front/js/pages/AdminPage.js
+++ src/front/js/pages/AdminPage.js
@@ -302,10 +302,10 @@ export const AdminPage = props => {
                  <button className="btn btn-primary" style ={{marginLeft: "5px"}} onClick={()=>statusFilter("Submitted")}>Submitted</button>
                  <button className="btn btn-primary" style ={{marginLeft: "5px"}} onClick={()=>statusFilter("Scanning")}>Scanning</button>
                  <button className="btn btn-primary" style ={{marginLeft: "5px"}} onClick={()=>statusFilter("Design")}>Design</button>
-                 <button className="btn btn-primary" style ={{marginLeft: "5px"}} onClick={()=>statusFilter("Manufacturing")}>Manufacturing</button>
-                 <button className="btn btn-primary" style ={{marginLeft: "5px"}} onClick={()=>statusFilter("Pre-Finish")}>PreFinish</button>
-                 <button className="btn btn-primary" style ={{marginLeft: "5px"}} onClick={()=>statusFilter("Finish")}>Finish</button>
-                 <button className="btn btn-primary" style ={{marginLeft: "5px"}} onClick={()=>statusFilter("Ready to Ship")}>Ready Ship</button>
+                 <button className="btn btn-primary" style ={{marginLeft: "5px"}} onClick={()=>statusFilter("Manufacturing")}>Manufact</button>
+                 <button className="btn btn-primary" style ={{marginLeft: "5px"}} onClick={()=>statusFilter("Pre-Finish")}>PreFin</button>
+                 <button className="btn btn-primary" style ={{marginLeft: "5px"}} onClick={()=>statusFilter("Finish")}>Fin</button>
+                 <button className="btn btn-primary" style ={{marginLeft: "5px"}} onClick={()=>statusFilter("Ready to Ship")}>Rdy Ship</button>
                  <button className="btn btn-primary" style ={{marginLeft: "5px"}} onClick={()=>statusFilter("Shipped")}>Shipped</button>
                  <button className="btn btn-primary" style ={{marginLeft: "5px"}} onClick={()=>statusFilter("Billed")}>Billed</button>
                  <button className="btn btn-primary" style ={{marginLeft: "5px"}} onClick={()=>statusFilter("Closed")}>Closed</button>

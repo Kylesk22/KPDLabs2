0ae6f04443f82b6101a6944ed794d6164a547b77
Kyle <kylesk22@gmail.com>
2024-08-21 10:38:52 -0400

    test

--- src/front/js/component/PrintScript.js
+++ src/front/js/component/PrintScript.js
@@ -72,7 +72,7 @@ export const PrintPDFButton = (props) => {
                     .info-data-3 {
                         margin-top: 20px;
                         font-size: 20px;
-                        width: 33%;
+                        text-align: left;
                         
                         
                     }

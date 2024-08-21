133d4848d2c1ea3f498e0a8420d1f2494f148c6f
Kyle <kylesk22@gmail.com>
2024-08-21 14:14:32 -0400

    test

--- src/front/js/component/PrintScript.js
+++ src/front/js/component/PrintScript.js
@@ -268,10 +268,11 @@ export const PrintPDFButton = (props) => {
                                 
                             </div>
                         </div>
-                        <div class="text-center>
+                        
+                    </div>
+                    <div class="text-center>
                             <div class="info-data-3">Case Notes: ${props.note}</div>
                         </div>
-                    </div>
                 </div>
                 <!-- Optionally add more content or load it dynamically -->
                 <script>

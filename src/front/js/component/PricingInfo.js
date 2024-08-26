7ea78526a3e1234b1102a589f932e807b1591877
Kyle <kylesk22@gmail.com>
2024-08-26 18:14:45 -0400

    Test

--- src/front/js/component/PricingInfo.js
+++ src/front/js/component/PricingInfo.js
@@ -278,7 +278,7 @@ export const PricingInfo = props => {
                 <div className="form-group mx-3">
                     
                         <label  htmlFor="findUs"><h5>Professional Position?</h5></label>
-                        <select style={{border: "black 1px solid"}} className="form-select" id="position"  style={{minHeight:"40px", backgroundColor:"white", border:"black 1px solid"}} aria-label="" onChange={(e)=>{setPosition(e.target.value)}}>
+                        <select  className="form-select" id="position"  style={{minHeight:"40px", backgroundColor:"white", border:"black 1px solid"}} aria-label="" onChange={(e)=>{setPosition(e.target.value)}}>
                             <option value="Select One">Select One</option>
                             <option value="Dentist" onClick={()=>setPosition("Dentist")}>Dentist</option>
                             <option value="Dental Office Manager" onClick={()=>setPosition("Dental Office Manager")}>Dental Office Manager</option>
@@ -293,7 +293,7 @@ export const PricingInfo = props => {
                 <div className="form-group mx-3">
                     
                         <label  htmlFor="findUs"><h5>How did you hear about us?</h5></label>
-                        <select style={{border: "black 1px solid"}} className="form-select" id="findUs"  style={{minHeight:"40px", backgroundColor:"white", border:"black 1px solid"}} aria-label="" onChange={(e)=>{setFindUs(e.target.value)}}>
+                        <select className="form-select" id="findUs"  style={{minHeight:"40px", backgroundColor:"white", border:"black 1px solid"}} aria-label="" onChange={(e)=>{setFindUs(e.target.value)}}>
                             <option value="Select One">Select One</option>
                             <option value="Google" onClick={()=>setFindUs("Google")}>Google</option>
                             <option value="Mailer" onClick={()=>setFindUs("Mailer")}>Mailer/Flyer</option>

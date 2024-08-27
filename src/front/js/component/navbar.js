50a0472e2cc4b6669d3204bf40747b2eed700d4f
Kyle <kylesk22@gmail.com>
2024-08-27 14:35:14 -0400

    test

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -151,6 +151,8 @@ export const Navbar = (props) => {
 
 	useEffect(()=>{
 		let token = getCookie('csrf_access_token')
+		const expirationCookie = getCookie(`${tokenName}-expires`);
+		console.log(`expired: ${expirationCookie}`)
 		console.log(`token: ${token}`)
 		console.log(`test: ${isTokenExpired(token)}`)
 		if (token !== null){

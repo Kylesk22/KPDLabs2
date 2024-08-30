b077156840c90549d45dd0b10b0683f3ad6d90e1
Kyle <kylesk22@gmail.com>
2024-08-30 14:49:11 -0400

    Test

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -721,7 +721,7 @@ className="">Cast Effective Digital Marketing Agency</h1>
 		</div>
 		
 		<div className="project-block col-lg-3 col-sm-6 wow fadeInRight">
-		<div style={{display: "flex", alignItems: "center"}} onClick={()=>{(cerecShow)? setCerecShow(false): setCerecShow(true)}} className="project-block col-lg-3 col-sm-6 wow fadeInRight" data-wow-delay="600ms">
+		<div style={{display: "flex", alignItems: "center"}} onClick={()=>{(cerecShow)? setCerecShow(false): setCerecShow(true)}} data-wow-delay="600ms">
 		  <div className="inner-box">
 			<div className="image-box" style={{height: "100px"}}>
 			  <figure className="image overlay-anim"><img src={Cerec} alt=""/></figure>
@@ -764,7 +764,7 @@ className="">Cast Effective Digital Marketing Agency</h1>
 		</div>
 		
 		<div className="project-block col-lg-3 col-sm-6 wow fadeInRight">
-		<div style={{display: "flex", alignItems: "center"}} onClick={()=>{(shapeShow)? setShapeShow(false): setShapeShow(true)}} className="project-block col-lg-3 col-sm-6 wow fadeInRight" data-wow-delay="800ms">
+		<div style={{display: "flex", alignItems: "center"}} onClick={()=>{(shapeShow)? setShapeShow(false): setShapeShow(true)}} data-wow-delay="800ms">
 		  <div className="inner-box">
 			<div className="image-box" style={{height: "100px"}}>
 			  <figure className="image overlay-anim"><img src={Shape} alt=""/></figure>
@@ -806,7 +806,7 @@ className="">Cast Effective Digital Marketing Agency</h1>
 		
 
 		<div className="project-block col-lg-3 col-sm-6 wow fadeInRight">
-		<div style={{display: "flex", alignItems: "center"}} onClick={()=>{(meditShow)? setMeditShow(false): setMeditShow(true)}} className="project-block col-lg-3 col-sm-6 wow fadeInRight" data-wow-delay="1000ms">
+		<div style={{display: "flex", alignItems: "center"}} onClick={()=>{(meditShow)? setMeditShow(false): setMeditShow(true)}} data-wow-delay="1000ms">
 		  <div className="inner-box">
 			<div className="image-box" style={{height: "100px"}}>
 			  <figure className="image overlay-anim"><img src={Medit} alt=""/></figure>

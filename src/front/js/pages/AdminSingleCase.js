53f9b39a69ac35a08aa2635c501b82a84db7fc83
Kyle <kylesk22@gmail.com>
2024-09-17 16:04:01 -0400

    test

--- src/front/js/pages/AdminSingleCase.js
+++ src/front/js/pages/AdminSingleCase.js
@@ -717,7 +717,7 @@ export const AdminSingleCase = props => {
                 // console.log(calculateBusinessDays(submissionDate, 6))
                 const result = calculateBusinessDays(6)
                 setResultDate(result)
-                console.log(result)
+                console.log(`result HERE ${result}`)
             },[submissionDate])
               
             

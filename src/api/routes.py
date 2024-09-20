dffa7cd3a28788a760943716d332901944dd4bf6
Kyle <kylesk22@gmail.com>
2024-09-20 14:35:46 -0400

    test

--- src/api/routes.py
+++ src/api/routes.py
@@ -102,6 +102,7 @@ CORS(app, supports_credentials=True)
 
 @app.route('/list_files/<folder>', methods=['GET'])
 def list_files(folder):
+    print(folder)
     response = s3.list_objects_v2(Bucket=SPACE_NAME, Prefix=f'{folder}/')
     files = [obj['Key'] for obj in response.get('Contents', [])]
     print(files)

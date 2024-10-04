1b2eca83587de0b4940cd88acc27138aa7f05c92
Kyle <kylesk22@gmail.com>
2024-10-04 11:30:25 -0400

    test

--- src/api/routes.py
+++ src/api/routes.py
@@ -685,6 +685,7 @@ def new_case(id):
                 update_case.status = "Submitted"
             else: 
                 update_case.status = status
+            update_case.add_log(f"{update_case.status}: {now_eastern.strftime('%m/%d/%Y %H:%M:%S')}")
             update_case.type = type
             update_case.gum_shade = gum_shade
             update_case.price = price

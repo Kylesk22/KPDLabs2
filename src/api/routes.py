8740da91c438c01621df3244db5149696690cf25
Kyle <kylesk22@gmail.com>
2024-10-07 09:09:47 -0400

    test

--- src/api/routes.py
+++ src/api/routes.py
@@ -652,8 +652,10 @@ def new_case(id):
             return jsonify({"msg": "Updated"}), 200 
 
         elif request.json.get("user", None) and request.json.get("logNote", None):
+            update_case = Case.query.filter_by(id=case).first()
             print("hit this line")
             update_case.add_log(f"Dr. {userobj.lname}: {request.json.get('logNote', None)}")
+            db.session.commit()
             return jsonify({"msg": "Updated"}), 200
         
         elif request.json.get("admin", None):

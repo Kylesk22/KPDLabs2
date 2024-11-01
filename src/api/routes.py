b7610b14d3c768b9c9d3f9ee4ea6dd33e4b4b9d7
Kyle <kylesk22@gmail.com>
2024-11-01 15:50:21 -0400

    test

--- src/api/routes.py
+++ src/api/routes.py
@@ -330,19 +330,19 @@ def getAllInfo(id):
 
         for case in all_cases:
             # Check if the case has a hold
-               if case.hold and case.hold_date_check is not now_eastern.strftime('%m/%d/%Y %H:%M:%S'):  # Assuming 'hold' is a string in 'MM/DD/YYYY HH:MM:SS' format
+               if case.hold and case.hold_date_check is not now_eastern.strftime('%m/%d/%Y'):  # Assuming 'hold' is a string in 'MM/DD/YYYY HH:MM:SS' format
                 hold_start_date = datetime.strptime(case.hold, '%m/%d/%Y %H:%M:%S')  # Convert to naive datetime
                 hold_start_date = hold_start_date.replace(tzinfo=eastern_offset)  # Localize to Eastern Time
 
                 hold_duration_days = (now_eastern - hold_start_date).days  # Calculate days since hold started
 
                 # Update the due date
-                if case.due_date and case.hold_date_check is not now_eastern.strftime('%m/%d/%Y %H:%M:%S'):  # Check if there's an existing due date
+                if case.due_date and case.hold_date_check is not now_eastern.strftime('%m/%d/%Y'):  # Check if there's an existing due date
                     case.hold_date_check = now_eastern.strftime('%m/%d/%Y %H:%M:%S')
                     original_due_date = datetime.strptime(case.due_date, '%m/%d/%Y %H:%M:%S')  # Convert to naive datetime
                     original_due_date = original_due_date.replace(tzinfo=eastern_offset)  # Localize to Eastern Time
                     new_due_date = original_due_date + timedelta(days=hold_duration_days)  # Add days on hold
-                    case.due_date = new_due_date.strftime('%m/%d/%Y %H:%M:%S')  # Update to desired format
+                    case.due_date = new_due_date.strftime('%m/%d/%Y')  # Update to desired format
 
                     # Save the updated due date back to the database
                     db.session.commit()

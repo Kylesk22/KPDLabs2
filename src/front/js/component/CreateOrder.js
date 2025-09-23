84f0dcd1bf9a4e55ea890a7c753842935e73da2d
Kyle <kylesk22@gmail.com>
2025-09-23 11:52:45 -0400

    test

--- src/front/js/component/CreateOrder.js
+++ src/front/js/component/CreateOrder.js
@@ -499,7 +499,7 @@ AWS.config.update({
     
     function toothHandler(e){
 
-        if (type === "denture" || type === "newDenture" || type === "dentureRepair" || type === "copyDenture"){
+        if (type === "denture" || type === "newDenture" || type === "dentureRepair" || type === "copyDenture" || type == "Night Guard"){
 
             const upperArch = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"];
             const lowerArch = ["18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];

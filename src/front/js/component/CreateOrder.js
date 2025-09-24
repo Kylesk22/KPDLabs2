c160280f6d05b38850b8edf999f8cec6ece9a172
Kyle <kylesk22@gmail.com>
2025-09-24 15:16:37 -0400

    test

--- src/front/js/component/CreateOrder.js
+++ src/front/js/component/CreateOrder.js
@@ -509,8 +509,8 @@ AWS.config.update({
             const upperArch = ["2","3","4","5","6","7","8","9","10","11","12","13","14","15"];
             const lowerArch = ["18","19","20","21","22","23","24","25","26","27","28","29","30","31"];
           
-            const ARCH_UPPER = "Upper Arch";
-            const ARCH_LOWER = "Lower Arch";
+            const ARCH_UPPER = " Upper Arch";
+            const ARCH_LOWER = " Lower Arch";
           
             const allTeeth = [...upperArch, ...lowerArch];
           

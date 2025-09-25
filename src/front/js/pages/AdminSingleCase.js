ae7153e026517c8b802826660ac022e2df4d29f3
Kyle <kylesk22@gmail.com>
2025-09-25 12:22:14 -0400

    test

--- src/front/js/pages/AdminSingleCase.js
+++ src/front/js/pages/AdminSingleCase.js
@@ -561,15 +561,38 @@ export const AdminSingleCase = props => {
                 // Update tooth colors
                 console.log(patientData.teeth)
                 
-                    let returnedTeeth = patientData.teeth
-                    const numberArray = returnedTeeth.replace(/[^\d,-]/g, '').split(',');;
-                    setCrownTooth(numberArray);
-                    for (let tooth in numberArray){
-                        const element = document.getElementById(numberArray[tooth]);
-                        
-                        element.style.fill = "#137ea7"
-                        
+                let returnedTeeth = patientData.teeth
+                // const numberArray = returnedTeeth.replace(/[^\d,-]/g, '').split(',');;
+                const upperArch = ["2","3","4","5","6","7","8","9","10","11","12","13","14","15"];
+                const lowerArch = ["18","19","20","21","22","23","24","25","26","27","28","29","30","31"];
+
+                const numberArray = returnedTeeth.replace(/[^\w\d,-\s]/g, '').split(',');
+                setCrownTooth(numberArray);
+
+                numberArray.forEach(tooth => {
+                    let teethToHighlight = [];
+                
+                    if (tooth === "Upper Arch") {
+                        teethToHighlight = upperArch;
+                    } else if (tooth === "Lower Arch") {
+                        teethToHighlight = lowerArch;
+                    } else {
+                        teethToHighlight = [tooth]; // single tooth
                     }
+                
+                    teethToHighlight.forEach(t => {
+                        const element = document.getElementById(t);
+                        if (element) {
+                            element.style.fill = "#137ea7";
+                        }
+                    });
+                });
+                // for (let tooth in numberArray){
+                //     const element = document.getElementById(numberArray[tooth]);
+                    
+                //     element.style.fill = "#137ea7"
+                    
+                // }
                 
                 // Fetch doctor data
                 const doctorResponse = await fetch(`${url}/${doctorId}`, options);

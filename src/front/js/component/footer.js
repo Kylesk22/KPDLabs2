e49c7f881586b5adfe4ac0771322552b4c6cd4bd
Kyle <kylesk22@gmail.com>
2026-06-03 11:06:43 -0400

    test

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,117 +1,89 @@
-import React, { Component } from "react";
-
-import FootBKG from "../../img/footer-bg.jpg"
-import Logo from "../../img/KPD-Transparent.png"
-import Denture from "../../img/Denture.jpg"
-
+import React from "react";
+import FootBKG from "../../img/footer-bg.jpg";
+import Logo from "../../img/kpd_logo_final.png";
 import "../../styles/home.css";
-import "../../styles/slick-theme.css"
-import "../../styles/slick.css"
-import "../../styles/style.css"
-import "../../styles/animate.css"
+import "../../styles/slick-theme.css";
+import "../../styles/slick.css";
+import "../../styles/style.css";
+import "../../styles/animate.css";
 import "../../styles/adminSingle.css";
 
 export const Footer = () => {
-	// Create a new Date object
-const currentDate = new Date();
+    const currentYear = new Date().getFullYear();
 
-// Get the current year
-const currentYear = currentDate.getFullYear();
+    return (
+        <footer className="main-footer no-print" style={{ backgroundImage: `url(${FootBKG})` }}>
+            <div className="bg-image"></div>
 
+            {/* Widgets */}
+            <div className="widgets-section">
+                <div className="auto-container">
+                    <div className="row">
 
+                        {/* Logo column */}
+                        <div className="footer-column col-lg-4 col-sm-6 wow fadeInLeft">
+                            <div className="footer-widget about-widget text-center text-lg-start">
+                                <div className="logo ps-4">
+                                    <a href="/"><img src={Logo} alt="KPD Labs" style={{ height: "70px", width: "111px" }} /></a>
+                                </div>
+                                <strong>
+                                    <span style={{ color: "white", fontSize: "12px" }}>Kronemeyer Precision</span>
+                                    <span style={{ color: "#ffaa17", fontSize: "12px" }}> Dental Laboratories, LLC</span>
+                                </strong>
+                            </div>
+                        </div>
 
+                        {/* Quick Links */}
+                        <div className="footer-column col-lg-4 col-sm-6 fadeInLeft">
+                            <div className="footer-widget contact-widget">
+                                <h3 className="widget-title">Quick Links</h3>
+                                <ul className="contact-info">
+                                    <li><i className="fa-solid fa-angle-right"></i> <a href="/aboutus">About Us</a></li>
+                                    <li><i className="fa-solid fa-angle-right"></i> <a href="/crownandbridge">Crown & Bridge</a></li>
+                                    <li><i className="fa-solid fa-angle-right"></i> <a href="/veneer">Veneer</a></li>
+                                    <li><i className="fa-solid fa-angle-right"></i> <a href="/partial">KPD Premier Partial</a></li>
+                                    <li><i className="fa-solid fa-angle-right"></i> <a href="/denture">KPD Premier Denture</a></li>
+                                    <li><i className="fa-solid fa-angle-right"></i> <a href="/removable-program">Removable Program</a></li>
+                                    <li><i className="fa-solid fa-angle-right"></i> <a href="/pricing">Pricing</a></li>
+                                    <li><i className="fa-solid fa-angle-right"></i> <a href="/contactus">Contact Us</a></li>
+                                </ul>
+                            </div>
+                        </div>
 
+                        {/* Contact */}
+                        <div className="footer-column col-lg-4 col-sm-6 fadeInLeft">
+                            <div className="footer-widget contact-widget">
+                                <h3 className="widget-title">Contact Now</h3>
+                                <div className="widget-content">
+                                    <div className="text">
+                                        <i className="fa-solid fa-location-dot"></i> 3393 US Hwy 17-92 West
+                                        <div className="footer-breakpoint"></div>Haines City, FL 33844
+                                    </div>
+                                    <ul className="contact-info">
+                                        <li><i className="fas fa-envelope"></i> <a href="mailto:kpdlabs@kpdlabs.com">kpdlabs@kpdlabs.com</a></li>
+                                        <li><i className="fas fa-phone"></i> <a href="tel:8634382109">863-438-2109</a></li>
+                                    </ul>
+                                    <ul className="social-icons justify-content-center">
+                                        <li><a href="https://www.facebook.com/profile.php?id=61559674932411&mibextid=LQQJ4d"><i className="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="https://www.instagram.com/kpd_labs?igsh=MzRlODBiNWFlZA=="><i className="fab fa-instagram"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
 
-return (
-	<footer className="main-footer no-print" style={{backgroundImage: `url(${FootBKG})`}}>
-  <div className="bg-image"  ></div>
-  {/* <!--Widgets Section--> */}
-  <div className="widgets-section">
-	<div className="auto-container">
-	  <div className="row"> 
-		{/* <!--Footer Column--> */}
-		<div className="footer-column col-lg-4 col-sm-6 wow fadeInLeft">
-		  <div className="footer-widget about-widget text-center text-lg-start">
-			<div className="logo ps-4"><a href="/"><img src={Logo} alt="" /></a></div>
-			<strong> <span style={{color: "white", fontSize: "12px"}}>Kronemeyer Precision</span><span style={{color: '#ffaa17', fontSize: "12px"}}> Dental Laboratories, LLC</span></strong>
-			{/* <div className="subscribe-form"> */}
-			  {/* <form method="post" action="#">
-				<div className="form-group">
-				  <input type="email" name="email" className="email"  placeholder="Your Address" required=""/>
-				  <button type="button" className="theme-btn"><i className="fa fa-paper-plane"></i></button>
-				</div>
-			  </form> */}
-			{/* </div> */}
-		  </div>
-		</div>
-		{/* <!--Footer Column--> */}
-		{/* <div className="footer-column col-lg-3 col-sm-6 wow fadeInLeft" data-wow-delay="400ms"> */}
-		  {/* <div className="footer-widget gallery-widget">
-			<h3 className="widget-title">Our Products</h3>
-			<div className="widget-content">
-			  <div className="outer clearfix">
-				<figure className="image"> <a href="#"><img src={Denture} alt=""/></a> </figure>
-				<figure className="image"> <a href="#"><img src={Denture} alt=""/></a> </figure>
-				<figure className="image"> <a href="#"><img src={Denture} alt=""/></a> </figure> */}
-				{/* <figure className="image"> <a href="#"><img src={Denture} alt=""/></a> </figure>
-				<figure className="image"> <a href="#"><img src={Denture} alt=""/></a> </figure>
-				<figure className="image"> <a href="#"><img src={Denture} alt=""/></a> </figure> */}
-			  {/* </div>
-			</div>
-		  </div> */}
-		{/* </div> */}
-		{/* <!--Footer Column--> */}
-		<div className="footer-column col-lg-4 col-sm-6 fadeInLeft" >
-		  <div className="footer-widget contact-widget">
-			<h3 className="widget-title">Quick Links</h3>
-			{/* <ul className="widget-content">
-			  <li><a href="/aboutus">About Us</a></li>
-			  <li><a href="#">Products</a></li>
-			  <li><a href="/contactus">Contact Us</a></li>
-			</ul> */}
-			<ul className="contact-info ">
-				<li><i className="fa-solid fa-angle-right"></i> <a href="/aboutus">About Us</a><br/>
-				</li>
-				<li><i className="fa-solid fa-angle-right"></i> <a href="#products">Products</a><br/>
-				</li>
-				<li><i className="fa-solid fa-angle-right"></i> <a href="/contactus">Contact Us</a><br/>
-				</li>
-			  </ul>
-		  </div>
-		</div>
-		{/* <!--Footer Column--> */}
-		<div className="footer-column col-lg-4 col-sm-6 fadeInLeft" >
-		  <div className="footer-widget contact-widget">
-			<h3 className="widget-title ">Contact Now</h3>
-			<div className="widget-content ">
-			  <div className="text"><i className="fa-solid fa-location-dot"></i>3393 US Hwy 17-92 West <div className="footer-breakpoint"></div>Haines City, FL 33844</div>
-			  <ul className="contact-info ">
-				<li><i className="fas fa-envelope"></i> <a href="mailto:kpdlabs@kpdlabs.com">kpdlabs@kpdlabs.com</a><br/>
-				</li>
-				<li><i className="fas fa-phone"></i> <a href="tel:8634382109">863-438-2109</a><br/>
-				</li>
-			  </ul>
-			  <ul className="social-icons justify-content-center">
-				<li><a href="https://www.facebook.com/profile.php?id=61559674932411&mibextid=LQQJ4d"><i className="fab fa-facebook-f"></i></a></li>
-				<li><a href="https://www.instagram.com/kpd_labs?igsh=MzRlODBiNWFlZA=="><i className="fab fa-instagram"></i></a></li>
+                    </div>
+                </div>
+            </div>
 
-			  </ul>
-			</div>
-		  </div>
-		</div>
-	  </div>
-	</div>
-  </div>
-  
-  {/* <!--Footer Bottom--> */}
-  <div className="footer-bottom" style={{zIndex:1}}/>
-	<div className="auto-container">
-	  <div className="inner-container">
-		<div className="copyright-text text-center">© {currentYear} KPD Labs Copyright All Rights Reserved 
-			{/* <a href="index.html">kodesolution.com</a> */}
-		<a href="#" className="footer-btn"></a>
-	  </div>
-	</div>
-  </div>
-</footer>
-)};
+            {/* Footer Bottom */}
+            <div className="footer-bottom" style={{ zIndex: 1 }} />
+            <div className="auto-container">
+                <div className="inner-container">
+                    <div className="copyright-text text-center">
+                        © {currentYear} KPD Labs. All Rights Reserved.
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
